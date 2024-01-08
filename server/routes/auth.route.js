a0cfd750fc2d194358ef19b12328b63d40a277a4
buimyy0409 <buimyy2909@gmail.com>
2024-01-08 20:44:08 +0700

    update

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -3,15 +3,13 @@ const router = express.Router();
 
 const {login, signup, verify} = require('../controllers/auth.controller');
 const authMiddleware = require('../middlewares/auth.mdw');
-const validationMiddleware = require('../middlewares/validation.mdw');
 
-const {signup: signupSchema, login: loginSchema} = require('../validations/auth.validation')
 
-router.post('/signup', validationMiddleware(signupSchema), signup )
+router.post('/signup', signup )
 
 
 // login
-router.post('/login', validationMiddleware(loginSchema), login)
+router.post('/login', login)
 
 // verify
 router.post('/verify', authMiddleware, verify)
